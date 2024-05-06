3ab2b14d85b2f32a62ea80a139c0cef8a8a75f30
Twill1007 <thomaswillford@gmail.com>
2024-05-06 15:55:36 -0700

    Consulting Page

--- src/app/work/consulting/page.js
+++ src/app/work/consulting/page.js
@@ -3,27 +3,66 @@ import Image from "next/image";
 import "../consulting/ConsultingPage.css";
 
 // Import your images
-import LrgPlaneLandImage from "../../../../public/LrgPlaneLand.jpg";
+import LrgPlaneLandImage from "../../../../public/PlaneLandSmall2.png";
 import Cessna182Image from "../../../../public/Cessna_182.jpg";
 
 function ConsultingPage() {
   return (
-    <div style={{ height: "100vh" }}>
+    <div>
       <div>
         <div className="imagesContainer">
-          <div className="imageContainer">
+          <div className="imageContainer1">
             <Image
               src={LrgPlaneLandImage}
+              objectFit="cover"
               layout="responsive"
               alt="Large Plane Landing"
             />
+
+            <h1
+              style={{
+                paddingTop: "150px",
+                fontSize: "70px",
+                justifyContent: "center",
+                alignSelf: "center",
+                textAlign: "center",
+                color: "black",
+                width: "50%",
+              }}
+            >
+              Consulting Services
+            </h1>
           </div>
-          <div className="imageContainer">
-            <Image
-              src={Cessna182Image}
-              layout="responsive"
-              alt="Small Plane Landing"
-            />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <div
+              style={{
+                width: "50%",
+                paddingLeft: "5%",
+                paddingRight: "5%",
+                alignContent: "center",
+                fontSize: "50px",
+              }}
+            >
+              proident Duis dolor minim incididunt non enim occaecat magna
+              veniam veniam cillum enim officia non occaecat non. nisi sit in
+              fugiat nisi elit aute commodo fugiat ut do labore amet voluptate
+              nostrud cillum mollit reprehenderit aliqua. eiusmod in consectetur
+              commodo aute esse non incididunt sit labore esse nostrud officia
+            </div>
+            <div className="imageContainer2">
+              <Image
+                src={Cessna182Image}
+                width={500}
+                height={500}
+                layout="responsive"
+                alt="Small Plane Landing"
+              />
+            </div>
           </div>
         </div>
       </div>
