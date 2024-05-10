8667ffde0891b32fcdf00a74d8d45dbc5923f528
Twill1007 <thomaswillford@gmail.com>
2024-05-10 14:47:26 -0700

    Updated Consulting Page

--- src/app/work/consulting/page.js
+++ src/app/work/consulting/page.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import "../consulting/ConsultingPage.css";
+import "@/app/work/consulting/consultingPage.css";
 
 // Import your images
 
